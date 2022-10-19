fc65807777e1467108f6f7550f2650f1cc6d97e0
cris-avila <cavila0790@gmail.com>
2022-10-19 18:40:46 -0300

    Cambios Index + Maqueta Discos

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,36 +1,28 @@
-const d = document
+let slideIndex = 1;
+showSlides(slideIndex);
 
-export default function carrousel() {
-    const $nextBtn = d.querySelector(".carrousel-btn.next"),
-    $prevBtn = d.querySelector(".carrousel-btn .prev"),
-    $slides = d.querySelectorAll(".carrousel-img");
-
-    let i = 0;
-
-    d.addEventListener("click", (e) => {
-        if(e.target===$prevBtn) {
-            e.preventDefault();
-            $slides[i].classList.remove("active");
-            i--;
-
-            if (i < 0) {
-                i = $slides.length -1;
-            }
-            
-            $slides[i].classList.add("active");
-        }
-
-        if(e.target===$nextBtn) {
-            e.preventDefault();
-            $slides[i].classList.remove("active");
-            i++;
-
-            if (i >= $slides.length) {
-                i = 0;
-            }
-            
-            $slides[i].classList.add("active");
-        }
-    })
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
