4595b778b23804afd9377707470e960fe2e60252
cris-avila <cavila0790@gmail.com>
2022-10-19 19:18:37 -0300

    Home + Discos Fin

---
+++ js/contacto.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('form');
+const nombre = document.getElementById('nombre');
+const telefono = document.getElementById('telefono');
+const tema = document.getElementById('tema');
+console.log(nombre + telefono + tema);
